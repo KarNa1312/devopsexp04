e4133d15dafa597d40aac145554bf932d5df72ba
KarNa1312 <kartiknaikwadi1312@gmail.com>
2025-09-09 16:26:30 +0530

    Fix HelloWorld.java with proper Java content

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
-echo public class HelloWorld { > HelloWorld.java
-echo     public static void main(String[] args) { >> HelloWorld.java
-echo         System.out.println("Hello, Jenkins World!"); >> HelloWorld.java
-echo         System.out.println("Build automated successfully!"); >> HelloWorld.java
-echo     } >> HelloWorld.java
-echo } >> HelloWorld.java
+﻿public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins World!");
+        System.out.println("Build automated successfully!");
+    }
+}
