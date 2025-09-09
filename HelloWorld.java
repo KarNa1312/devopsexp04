242878d3de822275b604d026553990fcd5798278
KarNa1312 <kartiknaikwadi1312@gmail.com>
2025-09-09 16:14:24 +0530

    Add HelloWorld.java for Jenkins automation

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+echo public class HelloWorld { > HelloWorld.java
+echo     public static void main(String[] args) { >> HelloWorld.java
+echo         System.out.println("Hello, Jenkins World!"); >> HelloWorld.java
+echo         System.out.println("Build automated successfully!"); >> HelloWorld.java
+echo     } >> HelloWorld.java
+echo } >> HelloWorld.java
